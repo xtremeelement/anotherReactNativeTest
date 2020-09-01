37fb467fc17518cfc99713201ad16af81b99f920
Web <xtremeelement@gmail.com>
2020-09-01 11:19:48 -0400

    added date and time pickers

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground, Button, TouchableOpacity } from 'react-native';
 import SVG from "./images/svg.js";
 import Waves from "./images/wave.svg";
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 export default function App() {
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
   const imgURL = require("./images/car.jpg");
   return (
@@ -15,6 +38,21 @@ export default function App() {
         <Text style={styles.text}>SSD International Inc.</Text>        
       </ImageBackground>      
       <View style={styles.showBox}>
+        <Text style={{textAlign: "center"}}>Reserve a spot for CCW Class</Text>
+        <Button title="Select Date" onPress={showDatepicker} buttonStyle={styles.select__button}/>
+        <Button title="Select Time" onPress={showTimepicker} buttonStyle={styles.select__button}/>
+        <TouchableOpacity style={styles.select__button} onPress={showDatepicker}><Text style={{color: 'white', textAlign: 'center'}}>Select Time</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.select__button} onPress={showTimepicker}><Text style={{color: 'white', textAlign: 'center'}}>Select Date</Text></TouchableOpacity>
+        {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
       </View>
       <StatusBar backgroundColor="white" />
     </View>
@@ -27,33 +65,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgrey',    
     alignItems: 'center',   
   },
-  image__style: {
-    flex: 0,
-    height: 198,    
+  image__style: {    
+    height: 200,
     alignSelf: 'stretch',
+    justifyContent: 'center'
   },
   text: {
     color: "white",
     marginTop: '20%',
     fontSize: 30,
     textAlign: 'center',
-    backgroundColor: 'black',
+    backgroundColor: 'rgba(0,0,0,0.5)',
     width: '80%',
     alignSelf: 'center'
     // textAlignVertical: 'center'
-  },
-  svgThing: {
-    marginTop: -15,
-    height: '100%',
-    width: '100%'
-  },
+  },  
   showBox:{
     backgroundColor: 'white',
     width: '90%',
-    height: '50%',
+    height: '100%',
     marginTop: -15,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     textAlign: 'right'
+  },
+  select__button:{
+    marginTop: 10,
+    height: 40,
+    backgroundColor: 'orange',
+    justifyContent: 'center'
+    
   }
 });
